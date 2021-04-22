d988983e879339b067895c37763e891d3a3e3488
Orlando Ortega <ortegaorlando2@gmail.com>
2021-04-22 15:19:18 -0500

    Siri example app heroku

---
+++ serve_data.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, Response, s
+from os.path import join, dirname
+import json
+from fetch_from_db import get_popular_artists
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
